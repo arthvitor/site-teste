3cecf88fdcd618871a6e6cc5727720105452de16
Vitor Arthur <61673650+arthvitor@users.noreply.github.com>
2023-03-13 21:50:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
    return "Meu nome é Vitor. Muito prazer"
+
+@app.route("/sobre")
+def sobre():
+   return "Aqui vai o conteúdo da página Sobre"
+
+@app.route("/contato")
+def contato():
+   return "Aqui vai o conteúdo da página Contato"
