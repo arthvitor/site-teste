3a0ed5eb1af9a6e92d1e302ce9d137f17cbf8ff7
Vitor Arthur <61673650+arthvitor@users.noreply.github.com>
2023-03-14 11:27:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,17 +18,4 @@ def sobre():
 
 @app.route("/contato")
 def contato():
-   return menu + "Aqui vai o conteúdo da página Contato"
-
-@app.route("/teste")
-def ultimas_promocoes():
-  scraper = ChannelScraper()
-  contador = 0
-  resultado = []
-  for message in scraper.messages("promocoeseachadinhos"):
-    contador += 1
-    texto = message.text.strip().splitlines()[0]
-    resultado.append(f"{message.created_at} {texto}")
-    if contador == 10:
-      return menu + resultado
-   
+   return menu + "Aqui vai o conteúdo da página Contato"   
