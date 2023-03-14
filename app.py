223d90e018b3aae647bef4305a81fd096d3a6732
Vitor Arthur <61673650+arthvitor@users.noreply.github.com>
2023-03-13 22:23:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 menu = '''
-<a href="/">Pagina Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a> 
+<a href="/">Pagina Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a> | <a href="/teste">Teste</a>
 <br>
 '''
 
@@ -18,3 +18,16 @@ def sobre():
 @app.route("/contato")
 def contato():
    return menu + "Aqui vai o conteúdo da página Contato"
+
+@app.route("/teste")
+def ultimas_promocoes():
+  scraper = ChannelScraper()
+  contador = 0
+  resultado = []
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    resultado.append(f"{message.created_at} {texto}")
+    if contador == 10:
+      return menu + resultado
+   
